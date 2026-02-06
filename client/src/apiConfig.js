91dbe116049a72f8d598680261426e3afe10d4e0
yhlee <dldydgus6302@naver.com>
2026-02-06 14:36:25 +0900

    "feat: 상용 API_BASE_URL 설정

--- client/src/apiConfig.js
+++ client/src/apiConfig.js
@@ -2,6 +2,6 @@
 // 로컬 개발 시: "http://localhost:5000"
 // 렌더 배포 후: "https://내-프로젝트-이름.onrender.com" (렌더에서 발급받은 주소로 변경하세요)
 
-const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL = "https://lunch-pirates-server.onrender.com";
 
 export default API_BASE_URL;
